c477f1fed00e2f49b9a622404e4c7d5ffc7f223f
Andrea <andreaboccone33@gmail.com>
2019-10-13 18:13:36 +0200

    migliorie di leggibiltà
    Descritto alla meno peggio le funzionalità del RedPatient con commenti lezzi.

--- RedPatient.java
+++ RedPatient.java
@@ -1,54 +1,53 @@
-public class RedPatient extends Thread{
+//Un paziente con codice rosso è un paziente che ha priorià massima ed ha bisogno di essere visitato
+//da tutti i 10 medici disponibili contemporaneamente per essere curato.
+public class RedPatient extends Patient {
 
-    Hospital prontoSoccorso;
-    private long visitTimeNeeded;
-    private int times;
-
-    public RedPatient(Hospital newProntoSoccorso){
-        this.prontoSoccorso = newProntoSoccorso;
-        this.times = 3;
-        this.visitTimeNeeded = 5000;
+    public RedPatient(Hospital newProntoSoccorso) {
+        super(newProntoSoccorso);
+        this.visitTimeNeeded += 2000;
         Thread.currentThread().setPriority(10);
     }
 
-    public void run(){
-        System.out.println("                                                    Sono rosso, " + Thread.currentThread().getName() + " e sono sveglio!");
+    public void run() {
         prontoSoccorso.lockMedici.lock();
         try {
-            //if (!prontoSoccorso.redPatientCanGoIn()) {
-                prontoSoccorso.redPatients++;
-            //}
-            while (!prontoSoccorso.redPatientCanGoIn()) {
-                try {
-                    prontoSoccorso.yourTurn.await();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            waitMyTurn();
+
             prontoSoccorso.redPatientGotIn();
-            System.out.println("E' entrato il paziente rosso " + Thread.currentThread().getName());
-        }
-        finally {
+            printPatientGotIn("rosso", -1);
+
+        } finally {
             prontoSoccorso.lockMedici.unlock();
         }
-
-        Main.pause(visitTimeNeeded);
-
+        //inizia la visita
+        MainClass.pause(visitTimeNeeded);
+        //fine della visita
         prontoSoccorso.lockMedici.lock();
         try {
-            System.out.println("E' uscito il paziente rosso " + Thread.currentThread().getName());
             prontoSoccorso.redPatientGettingOut();
-        }
-        finally{
+            times--;
+            printPatientGotOut("rosso", -1);
+        } finally {
             prontoSoccorso.lockMedici.unlock();
         }
 
-        times--;
-        if(times > 0){
-           Main.pause(1000);
-           run();
+        if (times > 0) {
+            MainClass.pause(2000);
+            run();
         }
     }
 
-
+    //Quando un paziente con codice rosso arriva, aumenta il contatore dei pazienti in attesa con tale codice, in modo da comunicare ad ogni
+    //altro paziente con priorità minore di non poter entrare a fare alcuna visita. In seguito, attende il suo turno,
+    //ovvero che i pazienti al momento in visita escano e rendano di nuovo disponibili i medici richiesti.
+    protected void waitMyTurn() {
+        prontoSoccorso.redPatients++;
+        while (!prontoSoccorso.redPatientCanGoIn()) {
+            try {
+                prontoSoccorso.yourTurn.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
